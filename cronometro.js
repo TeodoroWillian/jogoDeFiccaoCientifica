2b0f420ad01f08a04f7373283e74e77082cd91c1
TeodoroWillian <totawillian@gmail.com>
2022-05-08 19:56:06 -0400

    Faltando apenas terminar o for.

--- cronometro.js
+++ cronometro.js
@@ -1,17 +1,22 @@
 const prompt = require("prompt-sync")();
 
-let player = {
-  nome: prompt("Digite o nome do seu personagem: "),
-  vida: 100,
-  habilidadeEspecial: false,
-  escolhaFuga: false,
-  falhaNaFuga: function () {
-    return (this.vida = 0);
-  },
-  perdaDeVida: function () {
-    return (this.vida = this.vida - 10);
-  },
-};
+var inicio = "proximaFase04";
+var tempo = 4;
 
-player.perdaDeVida();
-console.log(player.vida);
+while (inicio == "proximaFase04") {
+  console.log("Você chegou a enfermaria.");
+  for (let i = 0; i < tempo == 8; i++) {
+    esperaAnoitecer = prompt(
+      "Deseja esperar anoitecer? para iniciar a fuga?"
+    ).toLocaleLowerCase();
+    if (esperaAnoitecer == "sim") {
+      tempo = tempo++;
+    } else {
+      break;
+    }
+    if (tempo == 8) {
+      break;
+    }
+  }
+  console.log(tempo);
+}
--- jogoIF.js
+++ jogoIF.js
@@ -5,20 +5,24 @@
 const prompt = require("prompt-sync")();
 
 let jogo = true;
+var tempo;
 
-let inicio = prompt("Deseja iniciar o jogo? ");
+var inicio = prompt("Deseja iniciar o jogo? ");
 
 if (inicio == "sim") {
   var player = {
     nome: prompt("Digite o nome do seu personagem: "),
     vida: 100,
-    habilidadeEspecial: false,
-    escolhaFuga: false,
     falhaNaFuga: function () {
       return (this.vida = 0);
     },
     perdaDeVida: function () {
-      return (this.vida = this.vida - 20);
+      return (this.vida = this.vida - 50);
+    },
+    verificaSeTemVida: function () {
+      if (this.vida == 0) {
+        console.log("Você morreu!");
+      }
     },
   };
 } else {
@@ -29,25 +33,16 @@ console.log(
   "Você está em uma cela em um presídio, seu objetivo é sair da prisão."
 );
 
-// inicia funções
+function contagemDoTempo() {
+  tempo = tempo++;
+}
+
 function esbarrarGuarda(probabilidade) {
   probabilidade = Math.floor(Math.random() * 3);
   return probabilidade;
 }
 
-function confereSePassouPeloGuarda(Esbarrar) {
-  if (Esbarrar == 0) {
-    player.falhaNaFuga();
-    console.log("O guarde te viu e te deteve, você falhou.");
-    inicio = prompt("Deseja jogar novamente?");
-  } else {
-    console.log("Muito bem, você conseguiu.");
-    inicio = "fase03";
-  }
-}
-
-//termina funções
-
+// inicia a primeira fase do jogo
 while (jogo == true) {
   while (inicio == "sim") {
     console.log(
@@ -61,47 +56,91 @@ while (jogo == true) {
         "Você acertou. Dica: O único caminho para para fugir do presídio é pela enfermaria!"
       );
       inicio = "proximaFase";
+      contagemDoTempo();
     } else {
       inicio = prompt("Você não acertou, deseja tentar novamente?");
+      tempo = 0;
     }
   }
-
   while (inicio == "proximaFase") {
-    let escolha;
     console.log(
       "Agora que você sabe que o único caminho é pela enfermaria, acertando a próxima questão, será liberado duas opções para chegar até a enfermaria."
     );
+    console.log();
     const anoSonho = prompt(
-      "Em qual ano foi lançado o filme: Um sonho de Liberdade?"
+      "Em qual ano foi lançado o filme: Um sonho de Liberdade? "
     );
+    console.log();
     if (anoSonho == 1995) {
       console.log(
         "Muito bem,você acertou! Você pode tentar chegar a enfermaria fingindo estar doente (escolha 1), ou pode tentar escondido durante o banho de sol (escolha 0)"
       );
+
+      inicio = "proximaFase02";
       console.log();
-      escolha = prompt("Escolha 01 ou 02 para prosseguir.");
     } else {
-      inicio = prompt("você errou, deseja reiniciar o jogo?");
-
+      inicio = prompt("você errou, deseja jogar novamente?");
+      tempo = 0;
     }
+    contagemDoTempo();
+  }
 
-    if (escolha == 1) {
+  while (inicio == "proximaFase02") {
+    var escolhaFase02 = prompt(
+      "escolha 1 para fingir estar doente ou 1 para entrar na enfermaria durante o banho de sol"
+    );
+
+    if (escolhaFase02 == 1) {
       console.log(
-        "Para prosseguir para a enfermaria e passar um grande tempo lá, você terá que se machucar, isso vai resultar na perda de 20 de sua vida."
+        "Você cometeu um ferimento a si mesmo para entrar na enfermaria, este movimento irá custa -50 de vida"
       );
       player.perdaDeVida();
-      inicio = "fase02";
-
-    } else if (escolha == 2) {
-      let parametro;
-      let resultadoEsbarrar = esbarrarGuarda(parametro);
-      confereSePassouPeloGuarda(resultadoEsbarrar);
+      inicio = "proximaFase03";
+    } else if (escolhaFase02 == 2) {
+      var esbarrou = esbarrarGuarda(esbarrou);
+      console.log(esbarrou);
     }
-
-    console.log(player.vida);
+    if (esbarrou == 0) {
+      player.falhaNaFuga();
+      inicio = prompt("O guarde te viu, você perdeu, deseja continuar?");
+      tempo = 0;
+    } else if (esbarrou != 0) {
+      console.log("Muito bem, você passou pelo guarda.");
+      inicio = "proximaFase03";
+    }
+    contagemDoTempo();
   }
-}
+  while (inicio == "proximaFase03") {
+    console.log(
+      "Parabéns, você passou mais uma fase, para seguir adiante e poder escolher opções para seguir em frente, você terá que acerta a próxima questão"
+    );
+    var escolhaFase04 = prompt(
+      "Qual o nome do irmão do Michael em Prison Break?"
+    );
+    if (escolhaFase04 == "lincoln") {
+      console.log("Parabéns, você acertou.");
+      inicio = "proximaFase04";
+    } else {
+      inicio = prompt("Você errou, deseja jogar novamente?");
+      tempo = 0;
+    }
+    contagemDoTempo();
+  }
 
-while (inicio = "fase02"){
-  
+  while (inicio == "proximaFase04") {
+    console.log("Você chegou a enfermaria.");
+    for (let i = 0; i < 5; i++) {
+      esperaAnoitecer = prompt(
+        "Deseja esperar anoitecer? para iniciar a fuga?"
+      ).toLocaleLowerCase();
+      if (esperaAnoitecer == "sim") {
+        tempo = tempo++;
+      } else {
+        break;
+      }
+      if (tempo == 8) {
+        break;
+      }
+    }
+  }
 }
